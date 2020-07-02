bcad6996182261ac898e683aeab1cb5d1c49e971
Ajay Narayan Shinde <63417102+ShindeAjay238@users.noreply.github.com>
2020-07-02 11:38:20 +0530

    spring mvc demmo

---
+++ SpringMvc/SpringMvcSetting/WebContent/WEB-INF/displayname.jsp
@@ -0,0 +1,27 @@
+<%@page import="java.util.Set"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Display Name</title>
+</head>
+<style>
+body
+{
+text-align: center;
+text-shadow: blue;
+color: orange;
+}
+</style>
+<body>
+Hello   
+${firstName}<br>
+${lastName}<br>
+${email}<br>
+${address}
+${contact}
+
+</body>
+</html>
---
+++ SpringMvc/SpringMvcSetting/src/org/studyeasy/MainController.java
@@ -0,0 +1,38 @@
+package org.studyeasy;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET )
+	public String welcome() {
+		return "hello";
+	}
+	
+	@RequestMapping("displayname")
+	public String disaplyname(HttpServletRequest request)
+	{
+		System.out.println("controller hit properlly");
+		String firstName=request.getParameter("firstName");
+		request.setAttribute("firstName", firstName);
+		String lastName=request.getParameter("lastName");
+		request.setAttribute("lastName", lastName);
+		
+		String email=request.getParameter("email");
+		request.setAttribute("email", email);
+		
+		String address=request.getParameter("address");
+		request.setAttribute("address", address);
+		
+		String contact=request.getParameter("contact");
+		request.setAttribute("contact", contact);
+		
+		return "displayname";
+	}
+
+}
